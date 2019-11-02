332baff3cd1d28601259ec40d5b66742893fd562
Leonardo Miraglia <leo.mi.me@gmail.com>
2019-11-02 17:33:22 -0300

    feature: Menu component created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from './components/Header'
+import Menu from './components/Menu'
+import './App.scss'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Menu />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Menu.scss'
+
+const Menu  = () => (
+    <ul className="menu">
+        <li className="selected">Artists</li>
+        <li>Albums</li>
+        <li>Tracks</li>
+    </ul>
+)
+
+export default Menu
