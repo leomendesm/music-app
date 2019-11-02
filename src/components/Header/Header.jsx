7c3d494188f37092a60072e17e7cdc7d049d0be2
Leonardo Miraglia <leo.mi.me@gmail.com>
2019-11-02 17:18:23 -0300

    feature: Menu component created

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Header.scss'
+const Header = () => (
+    <header className="header">
+        <p className="title">Music</p>
+    </header>
+)
+
+export default Header;
