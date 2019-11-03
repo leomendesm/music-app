425b6e8a61b6c636206f4b4e73c30e1391ad1e2f
Leonardo Miraglia <leo.mi.me@gmail.com>
2019-11-03 02:06:26 -0200

    fix: Import error conflict between jsx and json

--- src/containers/Artists/Artists.jsx
+++ src/containers/Artists/Artists.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Artist from "../../components/Artist";
 import './Artists.scss'
-import Data from './Artists.json'
+import Data from './Data.json'
 class Artists extends Component {
     render() {
         return (
