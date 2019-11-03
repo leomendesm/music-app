c5e56b70590408aa5eb4a4fc2bbe5d182601f315
Leonardo Miraglia <leo.mi.me@gmail.com>
2019-11-03 03:08:39 -0200

    fix: cors error

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,13 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://itunes.apple.com'
+    baseURL: 'https://itunes.apple.com',
+    headers: {
+        'Access-Control-Allow-Credentials' : true,
+        'Access-Control-Allow-Origin':'*',
+        'Access-Control-Allow-Methods':'GET',
+        'Access-Control-Allow-Headers':'application/json',
+    }
 });
 
 export default instance
